3a51a2ee7eedd3cfd7608c2cfdaf1d52a41732f9
assafsauer <as.sauer@gmail.com>
2024-03-08 05:50:02 +0100

    Update generate-yaml.sh

--- istio/{{cookiecutter.project_slug}}/helm/generate-yaml.sh
+++ istio/{{cookiecutter.project_slug}}/helm/generate-yaml.sh
@@ -8,14 +8,30 @@ else
     output_path="$script_path/$1"
 fi
 
-helm repo add stevehipwell https://stevehipwell.github.io/helm-charts/ || exit 1
-helm repo update || exit 1
+helm repo add istio https://istio-release.storage.googleapis.com/charts helm repo update
 
-helm template "{{ cookiecutter.component.name }}" stevehipwell/istio-operator \
+helm install istio-base istio/base -n istio-system
+
+helm template "{{ cookiecutter.component.name }}" istio/istio-control/istio-discovery  \
 --namespace "{{ cookiecutter.component.attributes.namespace }}" \
 --version "{{ cookiecutter.technical.chart_version }}" \
---include-crds \
 -f "$script_path/helm-values.yaml" \
 > "$output_path/istio-operator.yaml" || exit 1
+
+
+helm template "{{ cookiecutter.component.name }}"  istio/gateway  \
+--namespace "{{ cookiecutter.component.attributes.namespace }}" \
+--version "{{ cookiecutter.technical.chart_version }}" \
+-f "$script_path/helm-values.yaml" \
+> "$output_path/istio-operator.yaml" || exit 1
+
+helm template "{{ cookiecutter.component.name }}" istio/istio-control/istio-discovery  \
+--namespace "{{ cookiecutter.component.attributes.namespace }}" \
+--version "{{ cookiecutter.technical.chart_version }}" \
+-f "$script_path/helm-values.yaml" \
+> "$output_path/istio-operator.yaml" || exit 1
+ 
+
+ 
 
 
