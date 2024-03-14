8338a0a66279689cd3d5e53161269be50fbdcd31
assafsauer <as.sauer@gmail.com>
2024-03-14 08:50:22 +0100

    migration defintion

--- istio-migration/hooks/pre_gen_project.py
+++ istio-migration/hooks/pre_gen_project.py
@@ -1,4 +1,4 @@
-
+ # Check namespace
 {% for service in cookiecutter.components.values() if service.group == 'mesh' and service.type == 'istio' %}
 {{ cookiecutter.update({"__istio_namespace": service.attributes.namespace }) }}
 {% endfor %}
