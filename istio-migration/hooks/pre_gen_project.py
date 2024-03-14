d0e7ce73554611284d07b26e978efe76173f8139
assafsauer <as.sauer@gmail.com>
2024-03-14 09:11:41 +0100

    migration defintion

--- istio-migration/hooks/pre_gen_project.py
+++ istio-migration/hooks/pre_gen_project.py
@@ -3,6 +3,30 @@
 {{ cookiecutter.update({"__istio_namespace": service.attributes.namespace }) }}
 {% endfor %}
 
+# Initialize variables to store the first matching link #
+{% set ns = namespace(found_to=None, found_from=None) %}
+
+# Iterate through links_to looking for the first match and store it #
+{% for link in cookiecutter.links_to.values() %}
+    {% if link.group == "mesh-backend" and ns.found_to is none %}
+        {% set ns.found_to = link %}
+    {% endif %}
+{% endfor %}
+
+# Iterate through links_from looking for the first match and store it #
+{% for link in cookiecutter.links_from.values() %}
+    {% if link.group == "mesh-backend" and ns.found_from is none %}
+        {% set ns.found_from = link %}
+    {% endif %}
+{% endfor %}
+
+# Update the cookiecutter context with the found links, if any #
+{% if ns.found_to %}
+    {{ cookiecutter.update({"__backend_to": ns.found_to}) }}
+{% endif %}
+{% if ns.found_from %}
+    {{ cookiecutter.update({"__backend_from": ns.found_from}) }}
+{% endif %}
 
 
 
