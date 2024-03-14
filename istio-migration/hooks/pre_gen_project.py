0e374204631ee31fb2df0fc368d3c72a50a09ab5
assafsauer <as.sauer@gmail.com>
2024-03-14 08:42:10 +0100

    migration defintion

--- istio-migration/hooks/pre_gen_project.py
+++ istio-migration/hooks/pre_gen_project.py
@@ -1,26 +1,14 @@
+
 {% for service in cookiecutter.components.values() if service.group == 'mesh' and service.type == 'istio' %}
 {{ cookiecutter.update({"__istio_namespace": service.attributes.namespace }) }}
 {% endfor %}
 
+ # Check if there is an existing link from
+ {% for link in cookiecutter.links_to.values() if link.group == "mesh-backend" %}
+ {{ cookiecutter.update({"__backend_to": link }) }}
+ {% endfor %}
 
-# define components for links from adn to
-
-{% set backend_to = [] %}
-{% set backend_from = [] %}
-
-{# Iterate through links_to looking for connections from components in the backend group #}
-{% for link_name, link in cookiecutter.links_to.items() %}
-    {% if cookiecutter.components[link.component].group == 'backend' %}
-        {% do backend_to.append(link.component) %}
-    {% endif %}
-{% endfor %}
-
-{# Iterate through links_from looking for connections to components in the backend group #}
-{% for link_name, link in cookiecutter.links_from.items() %}
-    {% if cookiecutter.components[link.component].group == 'backend' %}
-        {% do backend_from.append(link.component) %}
-    {% endif %}
-{% endfor %}
-
-{# Update the cookiecutter context with the identified components #}
-{{ cookiecutter.update({"__backend_to": backend_to, "__backend_from": backend_from}) }}
+ # Check if there is an existing link from
+ {% for link in cookiecutter.links_from.values() if link.group == "mesh-backend" %}
+ {{ cookiecutter.update({"__backend_from": link }) }}
+ {% endfor %}
