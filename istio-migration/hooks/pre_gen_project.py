3cb3bf8ce57efbb65be03d61c5026b678acfca04
assafsauer <as.sauer@gmail.com>
2024-03-14 09:39:48 +0100

    migration defintion

--- istio-migration/hooks/pre_gen_project.py
+++ istio-migration/hooks/pre_gen_project.py
@@ -2,3 +2,15 @@
 {% for service in cookiecutter.components.values() if service.group == 'mesh' and service.type == 'istio' %}
 {{ cookiecutter.update({"__istio_namespace": service.attributes.namespace }) }}
 {% endfor %}
+
+ # Check if there is an existing link from
+ {% for link in cookiecutter.links_to.values() if link.group == "mesh-backend" and link.type == 'istio-migration-python' %}
+ {{ cookiecutter.update({"__backend_python_to": link }) }}
+ {% endfor %}
+
+ # Check if there is an existing link from
+ {% for link in cookiecutter.links_from.values() if link.group == "mesh-backend" and link.type == 'istio-migration-python' %}
+ {{ cookiecutter.update({"__backend_python_from": link }) }}
+ {% endfor %}
+
+
