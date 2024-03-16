05c5ab50805fe750a61e29d55aba8488fef7c438
assafsauer <as.sauer@gmail.com>
2024-03-16 07:55:28 +0100

    migration defintiondddd

--- istio/{{cookiecutter.project_slug}}/helm/generate-yaml.sh
+++ istio/{{cookiecutter.project_slug}}/helm/generate-yaml.sh
@@ -25,8 +25,20 @@ helm template "{{ cookiecutter.component.name }}"  istio/base  \
 -f "$script_path/helm-values-base.yaml" \
 > "$output_path/istio-base.yaml" || exit 1
 
+{%- if cookiecutter.component.attributes.kiali %}
+
+helm repo add kiali https://kiali.org/helm-charts
+helm repo update
+
+helm template "{{ cookiecutter.component.name }}"  kiali/kiali-server  \
+--namespace "{{ cookiecutter.component.attributes.namespace }}" \
+--version "1.81.0" \
+-f "$script_path/helm-values-kiali.yaml" \
+> "$output_path/istio-kiali.yaml" || exit 1
+
+{%- endif %}
  
 
- 
+
 
 
