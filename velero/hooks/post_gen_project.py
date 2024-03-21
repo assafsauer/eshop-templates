b6dbdb5b0b62e3ae83e1c275c066c8b64d1e58cd
assafsauer <as.sauer@gmail.com>
2024-03-21 19:47:05 +0100

    external source code

--- velero/hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-from stacktic import encrypt_sops_files
-from stacktic import remove_file
-
-if __name__ == '__main__':
-
-
-    # SOPS Encryption
-    if {{ cookiecutter.configuration.sops_enabled }}:
-        encrypt_sops_files({{ cookiecutter._metadata.secret_files }}, '{{ cookiecutter.configuration.sops_age_recipients }}')
